7990beb551b65985c43e4daabe7493089ba9597c
Ibrahim <ibehbudov21@mail.ru>
2022-09-04 20:57:29 +0400

    Update removeall.sh

--- docker/removeall.sh
+++ docker/removeall.sh
@@ -4,3 +4,7 @@ docker rm -f $(docker ps -qa) &&
 docker rmi -f $(docker images -aq) &&
 docker system prune &&
 docker system prune -a
+
+
+docker-compose build --no-cache &&
+docker-compose up -d --force-recreate 
